6e2e942faa40ee278f419f752db50975f1bf6fdf
1ajitDevops <leo.ajit1@gmail.com>
2025-07-02 01:37:12 +0530

    Update main.tf

--- Child/azurerm_Resource_group/main.tf
+++ Child/azurerm_Resource_group/main.tf
@@ -3,7 +3,3 @@ resource "azurerm_resource_group" "resource" {
   location = var.resource_location
 }
 
-resource "azurerm_resource_group" "resource1" {
-  name     = var.resource_group_name1
-  location = var.resource_location1
-}
